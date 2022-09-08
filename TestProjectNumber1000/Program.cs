7c3bfe76c7b62e111f8b8f0a83ff5d5b7ae6c347
cade-perfili <58614773+cade-perfili@users.noreply.github.com>
2022-09-08 19:25:21 -0400

    first goofy thing

---
+++ TestProjectNumber1000/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("LOL! Look at me go");
+
+
+    static void Silly(string[] args)
+    {
+        Console.WriteLine("Look at this dumb stuff");
+    }
+Console.WriteLine(Silly);
